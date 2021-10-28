e561d9a82816f206700486da420ca61bbc38e1d2
Canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-28 18:26:37 +0300

    vuelidate add

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuelidate from "vuelidate";
 
 import { BootstrapVue } from "bootstrap-vue";
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <div class="container">
-      <h2>Form Component</h2>
+  <div class="container">
+    <!-- <div class="container">
       <b-card bg-variant="light">
         <b-form-group
           label-cols-lg="3"
@@ -11,81 +10,123 @@
           class="mb-0"
         >
           <b-form-group
-            class="d-flex"
             label="Street:"
             label-for="nested-street"
-            label-cols-sm="2"
+            label-cols-sm="3"
             label-align-sm="right"
           >
             <b-form-input id="nested-street"></b-form-input>
           </b-form-group>
 
           <b-form-group
-            class="d-flex"
             label="City:"
             label-for="nested-city"
-            label-cols-sm="2"
+            label-cols-sm="3"
             label-align-sm="right"
           >
             <b-form-input id="nested-city"></b-form-input>
           </b-form-group>
 
           <b-form-group
-            class="d-flex"
             label="State:"
             label-for="nested-state"
-            label-cols-sm="2"
+            label-cols-sm="3"
             label-align-sm="right"
           >
             <b-form-input id="nested-state"></b-form-input>
           </b-form-group>
 
           <b-form-group
-            class="d-flex"
             label="Country:"
             label-for="nested-country"
-            label-cols-sm="2"
+            label-cols-sm="3"
             label-align-sm="right"
           >
             <b-form-input id="nested-country"></b-form-input>
           </b-form-group>
+
+          <b-form-group
+            label="Ship via:"
+            label-cols-sm="3"
+            label-align-sm="right"
+            class="mb-0"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-radio-group
+              class="pt-2"
+              :options="['Air', 'Courier', 'Mail']"
+              :aria-describedby="ariaDescribedby"
+            ></b-form-radio-group>
+          </b-form-group>
         </b-form-group>
-        <b-form inline class="mt-2">
-          <label class="mr-sm-2" for="inline-form-custom-select-pref"
-            >Ship via:</label
-          >
-          <b-form-select
-            id="inline-form-custom-select-pref"
-            class="mb-2 mr-sm-2 mb-sm-0"
-            :options="[
-              { text: 'Choose...', value: 'null' },
-              'Air',
-              'Courier',
-              'Mail',
-            ]"
-            :value="null"
-          ></b-form-select>
-
-          <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
-            >Use the same address for reciept</b-form-checkbox
-          >
-
-          <b-button @submit.prevent variant="primary">Submit</b-button>
-        </b-form>
       </b-card>
+    </div> -->
+
+    <div>
+      <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
+        <label class="form__label">Name</label>
+        <input class="form__input" v-model.trim="$v.name.$model" />
+      </div>
+      <div class="error" v-if="!$v.name.required">Field is required</div>
+      <div class="error" v-if="!$v.name.minLength">
+        Name must have at least {{ $v.name.$params.minLength.min }} letters.
+      </div>
+      <div class="error" v-if="!$v.name.maxLength">
+        Name must have maximum of {{ $v.name.$params.maxLength.max }} letters.
+      </div>
+      <div>
+        {{ $v.name.$model }}
+      </div>
+
+      <div class="form-group" :class="{ 'form-group--error': $v.age.$error }">
+        <label class="form__label">Age</label>
+        <input class="form__input" v-model.trim.lazy="$v.age.$model" />
+      </div>
+      <div class="error" v-if="!$v.age.between">
+        Must be between {{ $v.age.$params.between.min }} and
+        {{ $v.age.$params.between.max }}
+      </div>
+      <span tabindex="0">Blur to see changes</span>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import {
+  required,
+  minLength,
+  between,
+  maxLength,
+} from "vuelidate/lib/validators";
+
+import { global } from "@/mixins/global.js";
+
+export default {
+  data() {
+    return {
+      name: "",
+      age: 0,
+    };
+  },
+  mixins: [global],
+  validations: {
+    name: {
+      required,
+      minLength: minLength(6),
+      maxLength: maxLength(12),
+    },
+    age: {
+      between: between(20, 30),
+    },
+  },
+};
 </script>
 
 <style scoped>
-label {
+/* label {
   text-align: right;
   clear: both;
   float: left;
   margin-right: 15px;
-}
+} */
 </style>
