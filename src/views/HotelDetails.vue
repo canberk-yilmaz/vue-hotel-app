acf00f0f7656af2572a8cadb139d89418334f27b
canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-31 23:59:12 +0300

    datepicker add, days and payment functions create

--- src/views/HotelDetails.vue
+++ src/views/HotelDetails.vue
@@ -1,31 +1,69 @@
 <template>
-  <div>
-    <GoBack />
-    <section class="hotels">
+  <div class="mt-4">
+    <div class="main-img"></div>
+    <div class="container">
       <h1 class="title">
         {{ hotel.name }}
       </h1>
+      <div class="row align-items-start">
+        <div class="hotels mb-3 mb-lg-0 col-lg-8">
+          <div class="hotel-details">
+            <!-- TODO: BOOTSTRAP CAROUSEL MIGHT BE ADD HERE -->
+            <div class="img-container">
+              <img :src="hotel.image" :alt="hotel.name" />
+            </div>
+            <div>Rating: {{ hotel.rating }}</div>
+            <div class="d-flex align-left mt-5">{{ hotel.summary }}</div>
+            <p>
+              {{ hotel.location }}
+            </p>
+          </div>
+        </div>
+        <div class="sticky p-lg-0 col-lg-4 book-section">
+          <h2>Book Now</h2>
+          <div>
+            <label for="checkInDate">Check In</label>
+            <b-form-datepicker
+              id="checkIn"
+              v-model="checkInDate"
+              class="mb-2"
+            ></b-form-datepicker>
+          </div>
 
-      <div class="hotel-details">
-        <!-- TODO: BOOTSTRAP CAROUSEL MIGHT BE ADD HERE -->
-        <img :src="hotel.image" :alt="hotel.name" />
-        <p>$ {{ hotel.price }}</p>
-        <p>Rating: {{ hotel.rating }}</p>
-        <p>{{ hotel.summary }}</p>
-        <p>
-          {{ hotel.location }}
-        </p>
+          <div>
+            <label for="checkOutDate">Check Out</label>
+            <b-form-datepicker
+              id="checkOut"
+              v-model="checkOutDate"
+              class="mb-2"
+            ></b-form-datepicker>
+          </div>
+
+          <div class="rooms-container text-align mb-3">
+            <label class="rooms-label" for="rooms">Rooms:</label>
+            <input class="room-input" v-model="rooms" disabled />
+            <div>
+              <button class="room-btn mr-2" @click="rooms--">-</button>
+              <button class="room-btn" @click="rooms++">+</button>
+            </div>
+          </div>
+          <div class="bookNow d-flex align-center justify-content-center">
+            <button>
+              <router-link to="/reservation">Book Now</router-link>
+            </button>
+          </div>
+          <div class="totalPayment" v-if="totalPayment">
+            Total Payment: $ {{ totalPayment }}
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 <script>
 import data from "@/data.json";
-import GoBack from "@/components/GoBack";
 export default {
-  components: {
-    GoBack,
-  },
+  components: {},
   props: {
     name: {
       type: String,
@@ -34,12 +72,28 @@ export default {
   data() {
     return {
       hotelId: this.$route.params.id,
+      checkInDate: null,
+      checkOutDate: null,
+      rooms: 0,
     };
   },
-
   computed: {
     hotel() {
       return data.hotels.find((hotel) => hotel.id === this.hotelId);
+    },
+    totalPayment() {
+      return (
+        this.calculateDays(this.checkInDate, this.checkOutDate) *
+        this.rooms *
+        1000
+      );
+    },
+  },
+  methods: {
+    calculateDays(day1, day2) {
+      const checkIn = Date.parse(day1);
+      const checkOut = Date.parse(day2);
+      return (checkOut - checkIn) / 86400000;
     },
   },
 };
@@ -47,10 +101,7 @@ export default {
 
 <style scoped>
 img {
-  max-width: 600px;
-  height: auto;
   width: 100%;
-  max-height: 400px;
 }
 
 .title {
@@ -89,5 +140,40 @@ p {
   font-size: 25px;
   font-weight: bold;
   text-decoration: none;
+}
+
+.rooms-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.room-btn {
+  background-color: #2c5dff;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 32px;
+  width: 2rem;
+}
+
+.rooms-label {
+  margin: 0;
+}
+
+.totalPayment {
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+}
+.bookNow {
+  font-size: 2rem;
+  color: #2c3e50;
+}
+
+.room-input {
+  width: 3rem;
+  text-align: center;
 }
 </style>
