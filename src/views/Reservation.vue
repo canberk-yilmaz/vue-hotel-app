e505969086e897ee1f79118ba3a86ecc68baad9e
Canberk-yilmaz <cyilmaz089@gmail.com>
2021-11-01 18:27:22 +0300

    css changes

--- src/views/Reservation.vue
+++ src/views/Reservation.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <div v-for="item in 1" :key="item">
-      <TheForm />
+    <div class="d-flex justify-content-center">
+      <div class="form__container">
+        <div class="form__item" v-for="(item, formId) in 6" :key="formId">
+          <TheForm />
+          <div class="text-center">Person - {{ formId + 1 }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center mt-1 mb-5">
+      <b-btn>Continue To Payment</b-btn>
     </div>
   </div>
 </template>
@@ -17,4 +25,17 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form__container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 60%;
+}
+
+.form__item {
+  width: 25%;
+  margin: 1rem;
+}
+</style>
